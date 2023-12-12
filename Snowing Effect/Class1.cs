f33ecddb04b941e980f843c5c017f8dd95390a80
22rh0227 <146077017+22rh0227@users.noreply.github.com>
2023-12-12 07:33:43 +0000

    Add files via upload

---
+++ Snowing Effect/Class1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snowing_Effect
+{
+    public class Snow
+    {
+        private static Random rnd = new Random();
+        public struct Snowflake
+        {
+            public int x, y, v, layer;
+            public ConsoleColor color;
+            public bool falling;
+            public char symbol;
+        }
+        public static Snowflake RandomFlake()
+        {
+            Snowflake flake = new Snowflake();
+            flake.x = rnd.Next(0, Console.WindowWidth);
+            flake.v = rnd.Next(2, 9);
+            flake.y = flake.v - 1;
+            flake.layer = rnd.Next(0, 6);
+            if (flake.layer == 0)
+            {
+                flake.color = ConsoleColor.White;
+            }
+            else if (flake.layer < 3)
+            {
+                flake.layer = 1;
+                flake.color = ConsoleColor.Gray;
+            }
+            else
+            {
+                flake.layer = 2;
+                flake.color = ConsoleColor.DarkGray;
+            }
+            int storeRnd = rnd.Next(0, 4);
+            if (storeRnd == 0)
+            {
+                flake.symbol = '*';
+            }
+            else if (storeRnd == 1)
+            {
+                flake.symbol = '0';
+            }
+            else if (storeRnd == 2)
+            {
+                flake.symbol = '#';
+            }
+            else
+            {
+                flake.symbol = 'o';
+            }
+            flake.falling = true;
+            PrintSnowFlakes(flake);
+            return flake;
+        }
+        private static void PrintSnowFlakes(Snowflake flake)
+        {
+            Console.ForegroundColor = flake.color;
+            Console.SetCursorPosition(flake.x, flake.y);
+            Console.WriteLine(flake.symbol);
+        }
+        public static Snowflake IsFalling(Snowflake flake, List<List<int>> snowLevels, List<int> highestLevels)
+        {
+            Console.ForegroundColor = flake.color;
+            Console.SetCursorPosition(flake.x, flake.y);
+            Console.WriteLine(' ');
+            if (flake.y + flake.v >= highestLevels[flake.x])
+            {
+                while (flake.x != 0 && snowLevels[flake.layer][flake.x] < snowLevels[flake.layer][flake.x - 1] - 1)
+                {
+                    flake.x--;
+                }
+                while (flake.x != Console.WindowWidth - 1 && snowLevels[flake.layer][flake.x] < snowLevels[flake.layer][flake.x + 1] - 1)
+                {
+                    flake.x++;
+                }
+                snowLevels[flake.layer][flake.x]--;
+                if (snowLevels[flake.layer][flake.x] < highestLevels[flake.x])
+                {
+                    highestLevels[flake.x] = snowLevels[flake.layer][flake.x];
+                }
+                if (flake.layer != 0)
+                {
+                    if (snowLevels[flake.layer][flake.x] >= snowLevels[flake.layer - 1][flake.x])
+                    {
+                        return flake;
+                    }
+                }
+                if (flake.layer == 2)
+                {
+                    if (snowLevels[2][flake.x] >= snowLevels[0][flake.x])
+                    {
+                        return flake;
+                    }
+                }
+                
+                Console.SetCursorPosition(flake.x, snowLevels[flake.layer][flake.x]);
+                Console.Write('█');
+                flake = RandomFlake();
+            }
+            else
+            {
+                //Console.WriteLine();
+                //Console.WriteLine(": " + flake.y);
+                flake.y += flake.v;
+                //Console.WriteLine(": " + flake.y);
+                //Console.ReadKey();
+                Console.SetCursorPosition(flake.x, flake.y);
+                Console.WriteLine(flake.symbol);
+            }
+            return flake;
+        }
+    }
+}
---
+++ Snowing Effect/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snowing_Effect
+{
+    //public struct LayerPoint
+    //{
+    //    public int f, s, t;
+    //}
+    internal class Program
+    {
+        
+        
+        static void Main(string[] args)
+        {
+            Console.ReadKey(true);
+            Console.CursorVisible = false;
+            List<Snow.Snowflake> snowflakes = new List<Snow.Snowflake>();
+            List<List<int>> snowLevels = new List<List<int>>();
+            List<int> highestLevels = new List<int>();
+            Console.SetCursorPosition(0, Console.WindowHeight - 1);
+            Console.ForegroundColor = ConsoleColor.White;
+            for (int j = 0; j < 3; j++)
+            {
+                snowLevels.Add(new List<int>());
+                for (int i = 0; i < Console.WindowWidth; i++)
+                {
+                    if (j == 0)
+                    {
+                        highestLevels.Add(Console.WindowHeight - 1);
+                        Console.Write('█');
+                    }
+                    snowLevels[j].Add(Console.WindowHeight - 1);
+                }
+            }
+            for (int i = 0; i < 100; i++)
+            {
+                snowflakes.Add(Snow.RandomFlake());
+            }
+            while (true)
+            {
+                for (int i = 0; i < snowflakes.Count; i++)
+                {
+                    snowflakes[i] = Snow.IsFalling(snowflakes[i], snowLevels, highestLevels);
+                }
+                System.Threading.Thread.Sleep(100);
+            }
+            //Console.ReadLine();
+        }
+    }
+}
